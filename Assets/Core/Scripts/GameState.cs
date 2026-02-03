626fbbf8d497136ed965e8f5a86705a1e237fe82
Alexander Wahlstrom <Alewah09@hotmail.com>
2026-02-03 23:10:57 +0100

    Finished v1 of gamemanager, gamestate, ui script

--- Assets/Core/Scripts/GameState.cs
+++ Assets/Core/Scripts/GameState.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class GameState
 {
     //Simple gamestate with important fields (should even help with saving correctly later)
-    string cityName;
-    int dayCount;
-    bool paused;
-    float dayProgress;
-    int population;
-    int income;
-    int balance;
+    //I decided on no methods (handled through gameManager) and clean state for SRP
+    public string cityName;
+    public int dayCount;
+    public bool paused;
+    public float dayProgress;
+    public int population;
+    public int income;
+    public int balance;
+
+
 }
