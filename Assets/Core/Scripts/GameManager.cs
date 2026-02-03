f5586a9ef7c7de9a4573789602c38621725f1ba2
Alexander Wahlström <alewah09@hotmail.com>
2026-02-03 20:53:58 +0100

    progress on gamemanager

--- Assets/Core/Scripts/GameManager.cs
+++ Assets/Core/Scripts/GameManager.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{   
+{
 
-    GameState gameState;
+    public static GameManager instance;
+
+    public GameState gameState { get; private set; }
+
     // Awake is called when loading the script, good to ensure existing data like gamestate
     void Awake()    
     {
-        
+        if (instance != Null)
+        {
+            Destroy(gameObject);
+            return
+            // destroy?
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject)
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameState = new GameState();
         
     }
 
