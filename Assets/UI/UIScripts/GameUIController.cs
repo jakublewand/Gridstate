5cd49373d9aaa188f10b154491c13c3660697832
Alexander Wahlstrom <Alewah09@hotmail.com>
2026-02-24 20:09:14 +0100

    added slop buildings

--- Assets/UI/UIScripts/GameUIController.cs
+++ Assets/UI/UIScripts/GameUIController.cs
@@ -153,7 +153,7 @@ public class GameUIController : MonoBehaviour
         double cost = effects.cost;
         double maintenance = effects.maintenance;
 
-        var card = new VisualElement { name = "BuildingCard" };
+        var card = new Button { name = "BuildingCard" };
         card.AddToClassList("building-card");
 
         var header = new VisualElement();
@@ -162,12 +162,6 @@ public class GameUIController : MonoBehaviour
         var titleLabel = new Label { text = title ?? string.Empty };
         titleLabel.AddToClassList("card-title");
         header.Add(titleLabel);
-
-        var buildingImage = new VisualElement();
-        buildingImage.AddToClassList("card-image");
-        var houseImg = Resources.Load<Texture2D>("UI/img/house0");
-        if (houseImg) buildingImage.style.backgroundImage = new StyleBackground(houseImg);
-        header.Add(buildingImage);
 
         card.Add(header);
 
@@ -194,13 +188,10 @@ public class GameUIController : MonoBehaviour
         footer.AddToClassList("card-footer");
         var costLabel = new Label { text = $"${cost}k" };
         costLabel.AddToClassList("card-cost");
-        var buyBtn = new Button { text = "Buy" };
-        buyBtn.AddToClassList("card-buy-btn");
         footer.Add(costLabel);
-        footer.Add(buyBtn);
         card.Add(footer);
 
-        buyBtn.clicked += () => BuyButtonPressed(buildingDefinition);
+        card.clicked += () => BuyButtonPressed(buildingDefinition);
 
         return card;
     }
